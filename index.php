84a38fbe7de47fffe761ae8ff6e3d38b7d375871
Chrish Patel <78516579+Chrish1603@users.noreply.github.com>
2022-07-05 14:25:47 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="add content">
+  <meta name="keywords" content="add keywords, separate, with, commas">
+  <meta name="author" content="Chrish Patel">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> Carbon Emissions </title>
+	
+	<link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Lobster&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+	
+	<link rel="stylesheet" href="css/carbonemissions.css">
+</head>
+	
+	<body>
+		
+	<div class= "wrapper">
+	
+	<div class="box logo">
+		<a href="index.php">
+	<img src="img/logo(300px).png" width="300" height= "119" class= "center"
+	alt="carbon emmisions logo" />
+		</a>
+	</div>	<!-- / logo	-->
+	
+	<div class="box header">
+		<h2> Carbon Emmisions </h2>
+	</div>	<!-- / header -->
+		
+	<div class="box search-bar">	
+		<form action="https://google.com/search" method="get" target="_blank">
+			<input type="text" name="q" placeholder= "Search..." class= center>
+		</form>	
+	</div>	<!-- / search-bar -->
+	
+	<div class="box nav">
+		
+		<a href="information.php">Information</a> 
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="statistics.php">Statistics</a> 
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="solutions.php">Solutions</a> 
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="videos.php">Videos</a> 
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="contact.php">Contact Us</a> 
+		
+	</div>	<!-- / nav -->
+	
+	<div class="box main">
+	<img src="img/CO2image.png" class="responsive"
+	alt= "CO2 image" />
+	</div>	<!-- / main	-->
+	
+	<div class="box side">
+	<h2>Useful Links</h2>
+		
+	<p>
+		The following links and information is obtained from Brittanica, NASA and Carbon Footprint
+	</p>
+	
+		<ul>
+			<li><a href=https://www.britannica.com/science/carbon-dioxide> 
+			What is Carbon Dioxide? </a></li>
+			
+			<li><a href=https://www.britannica.com/science/water>
+			What is Water Vapour?</a></li>
+			
+			<li><a href=https://www.britannica.com/science/methane>
+			What is Methane?</a></li>
+			
+			<li><a href=https://www.britannica.com/science/nitrous-oxide>
+			What is Nitrous Oxide?</a></li>
+			
+			<li><a href=https://climate.nasa.gov>
+			What is Climate Change?</a></li>
+			
+			<li><a href=https://www.carbonfootprint.com>
+			Carbon Footprint</a></li>
+			
+		</ul>
+	</div>	<!-- / side	-->
+	
+	<div class="box footer">
+	&copy; 2021 Designed by Chrish Patel for Digital Technologies 1.4 AS91880 
+	</div>	<!-- / footer -->
+		
+	</div> <!-- / wrapper -->
+		
+	</body>
+</html>
