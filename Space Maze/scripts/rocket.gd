26dbb46f800dc84bbf8445b30521e1595618f78b
Chrish1603 <78516579+Chrish1603@users.noreply.github.com>
2023-06-01 09:13:58 +1200

    rotate rocket, and WASD

---
+++ Space Maze/scripts/rocket.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@export var speed = 600
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed ("ui_up"):
+		velocity.y -= 1
+		rotation_degrees = 315
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		rotation_degrees = 135
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		rotation_degrees = 225
+	if Input.is_action_pressed ("ui_right"):
+		velocity.x += 1
+		rotation_degrees = 45
+
+	if velocity. length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+# f
