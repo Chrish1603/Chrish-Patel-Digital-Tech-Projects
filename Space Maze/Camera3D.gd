fb2ffe025d6acfe9df4276778d3d553ac125195c
Chrish1603 <78516579+Chrish1603@users.noreply.github.com>
2023-07-11 20:03:39 +1200

    player rolling, camera, lighting + environment

---
+++ Space Maze/Camera3D.gd
@@ -0,0 +1,26 @@
+extends Camera3D
+
+@export
+var target: NodePath
+@export
+var enabled := true
+@export_range(0, 1, 0.01)
+var speed := 3.0
+
+func _process(delta):
+	var target_node = get_node(target)
+	if not enabled or not target_node:
+		return
+	
+	var move_delta = speed * delta
+	var target_transform = target_node.global_transform
+	global_transform = global_transform.interpolate_with(target_transform, move_delta)
+	if target_node is Camera3D and target_node.projection == projection:
+		var new_near = lerp(near, target_node.near, move_delta)
+		var new_far = lerp(far, target_node.far, move_delta)
+		
+		if target_node.projection == PROJECTION_ORTHOGONAL:
+			set_orthogonal(lerp(size, target_node.size, move_delta), new_near, new_far)
+		else:
+			set_perspective(lerp(fov, target_node.fov, move_delta), new_near, new_far)
+			
---
+++ Space Maze/scripts/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody3D
+
+const SPEED = 6.0
+const ROTSPEED = 7
+
+func _ready():
+	pass
+
+@warning_ignore("unused_parameter")
+func _physics_process(delta):
+	
+	if Input.is_action_pressed( "ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		$MeshInstance3D.rotate_z(deg_to_rad(-ROTSPEED))
+	elif Input. is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		$MeshInstance3D.rotate_z(deg_to_rad(ROTSPEED))
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input. is_action_pressed( "ui_up") and Input. is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+		$MeshInstance3D.rotate_x(deg_to_rad(-ROTSPEED))
+	elif Input. is_action_pressed("ui_down"):
+		velocity.z = SPEED
+		$MeshInstance3D.rotate_x(deg_to_rad(ROTSPEED))
+	else:
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
+	
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity.y -= gravity * delta
+
+	# Handle Jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+	
