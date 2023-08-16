af97e50d9feb5874ea0a7b4e320ea6849a8e0e05
Chrish1603 <78516579+Chrish1603@users.noreply.github.com>
2023-08-17 10:08:57 +1200

    modifications 2

--- Space Maze/Button_title_play.gd
+++ Space Maze/Button_title_play.gd
@@ -1,9 +1,9 @@
 extends Button
 
+var speed: float = 0.01
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
